a582e4642b2833baf27d7fa902abdddce92afae5
Katrina Hernandez <abqkatrina@gmail.com>
2020-04-05 15:16:34 -0600

    big changes
    create-react-app

---
+++ src/js/Contacts.js
@@ -0,0 +1,35 @@
+// <!-- Contact -->
+
+			<div class="container medium">
+				<header>
+					<h2>Tell me what you think!</h2>
+					<p>I'll be ready for a developer job this July -- get me while I'm hot!</p>
+				</header>
+				<div class="row">
+						<form method="post" action="#">
+							<div class="row">
+									<input type="text" name="name" id="name" placeholder="Name" />
+									<input type="text" name="email" id="email" placeholder="Email" />
+									<input type="text" name="subject" id="subject" placeholder="Subject" />
+									<textarea name="message" id="message" placeholder="Message"></textarea>
+									<ul class="actions">
+										<li><input type="submit" value="Email Me"/></li>
+										<li><input type="reset" value="Clear Form" class="alt" /></li>
+									</ul>
+							</div>
+						</form>
+					</div>
+					<div class="">
+						<hr />
+						<h3>Find me on ...</h3>
+						<ul class="social">
+							<li><a href="http://www.github.com/abqkatrina" class="link github"><img src="img/githublogo.png" class="icon"/></a></li>
+							<li><a href="http://www.linkedin.com/in/abqkatrina" class="link linkedin-in"><img src="img/linkedin.png" class="icon"/></a></li>
+							{/* <li><a href="#" class="link twitter"><img src="img/twitter.png" class="icon"/></a></li> */}
+							{/* <li><a href="#" class="link facebook-f"><img src="img/facebook.png" class="icon"/></a></li> */}
+							{/* <li><a href="#" class="link skype"><img src="img/skype.png" class="icon"/></a></li> */}
+							{/*<li><a href="#" class="link instagram"><img src="img/instagram.png" class="icon"/></a></li> */}
+						</ul>
+						<hr />
+					</div>
+				</div>
