f973c8fe8a9efeaf3e1cab0c99415af61f255487
Katrina Hernandez <abqkatrina@gmail.com>
2020-06-12 10:06:08 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import logo from './img/isolated-monochrome-white.svg';
 import './less/app.less';
-import Nav from './Components/Nav.js';
-import Header from './Components/Header.js';
-import Skills from './Components/Skills.js';
-import Contacts from './Components/Contacts.js';
-import Portfolio from './Components/Portfolio.js';
-import Footer from './Components/Footer.js';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
         
           abqkatrina coming soon...
       
